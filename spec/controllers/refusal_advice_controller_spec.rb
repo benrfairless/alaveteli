764f595f9859473055d44d9bedd99afe053aeee0
Graeme Porteous <graeme@rgbp.co.uk>
2021-01-20 08:17:58 +0000

    Update refusal advice wizard HTML
    If there is a InfoRequest present then this will add a form which will
    submit wizard answers, suggestion and selected action to the refusal
    advice controller.

--- spec/controllers/refusal_advice_controller_spec.rb
+++ spec/controllers/refusal_advice_controller_spec.rb
@@ -2,9 +2,65 @@ require 'spec_helper'
 
 RSpec.describe RefusalAdviceController do
   describe 'POST #create' do
-    it 'returns a success' do
-      post :create
-      expect(response).to be_successful
+    let(:params) do
+      {
+        refusal_advice: {
+          questions: {
+            exemption: 'section-12', question_1: 'no', question_2: 'yes'
+          },
+          actions: {
+            internal_review: { action_1: 'false', action_2: 'true' },
+            clarification: { action_3: 'true', action_4: 'false' },
+            new_request: { action_5: 'false' }
+          },
+          button: 'clarification'
+        }
+      }
+    end
+
+    context 'valid params' do
+      it 'returns a success' do
+        post :create, params: params
+        expect(response).to be_successful
+      end
+
+      it 'parses params correctly' do
+        post :create, params: params
+
+        expect(assigns(:params).to_h).to match(
+          questions: {
+            exemption: 'section-12', question_1: 'no', question_2: 'yes'
+          },
+          actions: {
+            internal_review: { action_1: false, action_2: true },
+            clarification: { action_3: true, action_4: false },
+            new_request: { action_5: false }
+          },
+          button: 'clarification'
+        )
+      end
+    end
+
+    context 'invalid params' do
+      let(:invalid_params) do
+        params.deep_merge(refusal_advice: { invalid: true })
+      end
+
+      it 'raises error' do
+        expect { post :create, params: invalid_params }.to raise_error(
+          ActionController::UnpermittedParameters
+        )
+      end
+    end
+
+    context 'missing params' do
+      let(:missing_params) {}
+
+      it 'raises error' do
+        expect { post :create, params: missing_params }.to raise_error(
+          ActionController::ParameterMissing
+        )
+      end
     end
   end
 end
